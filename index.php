8f0f8fbef3c00da9fcb54cfbdbd912de2575bb13
Merlisagita <mer12mermer@gmail.com>
2026-04-21 17:55:00 +0800

    upload tugas boostrap biodata

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$namaDepan = "Merli";
+$namaBelakang = "Sagita";
+$umur = 18;
+$pekerjaan = "mahasiswa";
+echo "<br>";
+echo $namaDepan. " ". $namaBelakang. " ". $umur. " ". $pekerjaan;
+
+echo "<br>";
+echo "Halo! perkenalkan nama saya ". $namaDepan . " " .$namaBelakang. " . saya berusia " . $umur ." dan pekerjaan saya adalah " . $pekerjaan . " di universitas hamzanwadi";
+?>
